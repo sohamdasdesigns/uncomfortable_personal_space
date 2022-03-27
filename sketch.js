4d53c3debec427ea874de8bbebec80a3630e60f8
sohamdasdesigns <88555443+sohamdasdesigns@users.noreply.github.com>
2022-03-27 18:17:51 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ function draw() {
 
 function changeLength() {
   c = 40;
-  select('canvas').elt.style.letterSpacing = "26px";
+  select('canvas').elt.style.letterSpacing = "28px";
 }
 function changeLengthBack() {
   c = 0;
